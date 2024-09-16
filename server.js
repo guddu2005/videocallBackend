ebfcd5514409813f473acd54faaa2c1a6640f5d9
Guddu Kumar <gkazad112006@gmail.com>
2024-09-16 22:37:04 +0530

    videocallBackend

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const { Server } = require("socket.io");
+const http = require("http");
+
+const PORT = 8000;
+const app = express();
+
+// Create an HTTP server by passing the Express app to the http module
+const httpServer = http.createServer(app);
+
+// Attach Socket.IO to the HTTP server
+const io=new Server(httpServer,{
+  cors:true,
+})
+// Start the HTTP server
+httpServer.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`);
+});
+
+//Video Mapping 
+const emailToSocketIdMap = new Map();
+const socketidToEmailMap = new Map();
+
+io.on("connection", (socket) => {
+  console.log(`Socket Connected`, socket.id);
+  socket.on("room:join", (data) => {
+    const { email, room } = data;
+    emailToSocketIdMap.set(email, socket.id);
+    socketidToEmailMap.set(socket.id, email);
+    io.to(room).emit("user:joined", { email, id: socket.id });
+    socket.join(room);
+    io.to(socket.id).emit("room:join", data);
+  });
+
+  socket.on("user:call", ({ to, offer }) => {
+    io.to(to).emit("incomming:call", { from: socket.id, offer });
+  });
+
+  socket.on("call:accepted", ({ to, ans }) => {
+    io.to(to).emit("call:accepted", { from: socket.id, ans });
+  });
+
+  socket.on("peer:nego:needed", ({ to, offer }) => {
+    console.log("peer:nego:needed", offer);
+    io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
+  });
+
+  socket.on("peer:nego:done", ({ to, ans }) => {
+    console.log("peer:nego:done", ans);
+    io.to(to).emit("peer:nego:final", { from: socket.id, ans });
+  });
+});
